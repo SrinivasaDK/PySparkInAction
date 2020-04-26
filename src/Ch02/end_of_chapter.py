a4b02ac574625accf2387eedd348df335e7ec819
Jonathan Rioux <Jonathan_Rioux@epam.com>
2020-04-26 16:14:39 -0400

    Added some material for chapter 4 and 6.

--- src/Ch02/end_of_chapter.py
+++ src/Ch02/end_of_chapter.py
@@ -1,4 +1,4 @@
-##end-of-chapter.py############################################################
+# end-of-chapter.py############################################################
 #
 # Use this to get a free pass from Chapter 2 to Chapter 3.
 #
@@ -21,6 +21,8 @@ words = lines.select(explode(col("line")).alias("word"))
 
 words_lower = words.select(lower(col("word")).alias("word_lower"))
 
-words_clean = words_lower.select(regexp_extract(col("word_lower"), "[a-z]*", 0).alias("word"))
+words_clean = words_lower.select(
+    regexp_extract(col("word_lower"), "[a-z]*", 0).alias("word")
+)
 
 words_nonull = words_clean.where(col("word") != "")
